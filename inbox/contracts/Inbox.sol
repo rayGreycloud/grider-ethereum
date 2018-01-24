356f0a16a9ac9ed7ee2e6153d8e6b91508d4fa24
rayGreycloud <rayGreycloud@gmail.com>
2018-01-24 13:28:39 -0800

    Add contract folder

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -10,10 +10,4 @@ contract Inbox {
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-    
-    // Duplicates message() function automatically generated
-    // by public variable declaration 
-    function getMessage() public view returns (string) {
-        return message;
-    }
 }
