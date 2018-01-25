efb03d8bdb3fecd6374dff484367a89265a3c540
rayGreycloud <rayGreycloud@gmail.com>
2018-01-24 18:59:20 -0800

    Add test to verify initial message

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,13 +2,15 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 // Create new instance of Web3
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 // Import compiled contract 
 const { interface, bytecode } = require('../compile');
 
 // Initialize 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!';
 
 beforeEach(async () => {
   // Get a list of all accounts 
@@ -18,13 +20,20 @@ beforeEach(async () => {
     // tells web3 contract methods
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     // tells web3 to deploy new copy of contract 
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
     // tells web3 to send transaction to create contract
-    .send({ from: accounts[0], gas: '1000000' });  
+    .send({ from: accounts[0], gas: '1000000' }); 
+    
+    inbox.setProvider(provider); 
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {
+  it('should deploy a contract', () => {
     assert.ok(inbox.options.address);
+  });
+  
+  it('should have a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
   });
 });
